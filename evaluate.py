4457f8b0d11b1bbc44e50d1689c714012ec81752
fleer <sascha.fleer@googlemail.com>
2017-11-20 14:39:52 +0100

    evaluation script updated

--- evaluate.py
+++ evaluate.py
@@ -20,63 +20,6 @@ else:
           "E.g. python evaluate.py run_mnist ./model/001-network"
     sys.exit(0)
 
-
-def glimpseSensor(self, img, normLoc):
-    assert not np.any(np.isnan(normLoc))," Locations have to be between 1, -1: {}".format(normLoc)
-    assert np.any(np.abs(normLoc)<=1)," Locations have to be between 1, -1: {}".format(normLoc)
-
-    #loc = np.around(((normLoc + 1) / 2.0) * self.mnist_size) # normLoc coordinates are between -1 and 1
-    loc = normLoc * (self.unit_pixels * 2.)/ self.mnist_size # normLoc coordinates are between -1 and 1
-    # Convert location [-1,1] into MNIST Coordinates:
-    loc = np.around(((loc + 1) / 2.0) * self.mnist_size) # normLoc coordinates are between -1 and 1
-    #print "Default: {}".format(loc)
-    #print "unit Pixels: {}".format(loc1)
-    loc = loc.astype(np.int32)
-
-    img = np.reshape(img, (self.batch_size, self.mnist_size, self.mnist_size, self.channels))
-
-    zooms = []
-
-    # process each image individually
-    for k in xrange(self.batch_size):
-        imgZooms = []
-        one_img = img[k,:,:,:]
-        max_radius = self.mnist_size * (self.minRadius ** (self.depth - 1))
-        offset = 2 * max_radius
-
-        # pad image with zeros
-        one_img = self.pad_to_bounding_box(one_img, offset, offset, \
-                                           max_radius * 4 + self.mnist_size, max_radius * 4 + self.mnist_size)
-
-        for i in xrange(self.depth):
-            r = int(self.mnist_size * (self.minRadius ** (i - 1)))
-
-            d_raw = 2 * r
-            d = np.reshape(np.asarray(d_raw), [1])
-
-            d = np.tile(d, [2])
-
-            loc_k = loc[k,:]
-            adjusted_loc = offset + loc_k - r
-
-
-            one_img2 = np.reshape(one_img, (one_img.shape[0], \
-                                            one_img.shape[1]))
-
-            # crop image to (d x d)
-            #zoom = slice(one_img2, adjusted_loc, d)
-            zoom = one_img2[adjusted_loc[0]:adjusted_loc[0]+d[0], adjusted_loc[1]:adjusted_loc[1]+d[1]]
-            assert not np.any(np.equal(zoom.shape, (0,0))), "Picture has size 0, location {}, depth {}".format(adjusted_loc, d)
-
-            imgZooms.append(zoom)
-
-        zooms.append(np.stack(imgZooms))
-
-    zooms = np.stack(zooms)
-
-    return zooms
-
-
 save = True
 
 mnist_size = MNIST_DOMAIN_OPTIONS.MNIST_SIZE
@@ -130,12 +73,10 @@ sample_loc = np.tanh(np.random.normal(loc, loc_std, loc.shape))
 for n in range(nGlimpses):
     zooms = mnist.glimpseSensor(X,sample_loc)
     ng = 1
-    #for g in zooms:
     for g in range(batch_size):
         nz = 1
         plt.title(Y[g], fontsize=40)
         for z in zooms[g]:
-        #for z in g:
             plt.imshow(z[:,:], cmap=plt.get_cmap('gray'),
                        interpolation="nearest")
 
