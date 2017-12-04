a271c18b70b337e8818d3702ad54a38060be1814
fleer <sascha.fleer@googlemail.com>
2017-12-04 12:07:23 +0100

    Start location now choosen by the network

--- network.py
+++ network.py
@@ -121,6 +121,22 @@ class RAM():
                                 # bias_initializer=keras.initializers.glorot_uniform(),
                                  name='location_output',
                                  )(model_output)
+
+        #   ================
+        #   Location Network at timestep 0
+        #   ================
+
+        #TODO: Find a better solution
+
+        hidden_state_in_0 = keras.layers.Input(shape=(256,))
+        location_out_t0 = keras.layers.Dense(2,
+                                          activation=self.hard_tanh,
+                                          #kernel_initializer=keras.initializers.glorot_uniform(),
+                                          kernel_initializer=keras.initializers.RandomUniform(minval=-0.1, maxval=0.1),
+                                          bias_initializer=keras.initializers.RandomUniform(minval=-0.1, maxval=0.1),
+                                          # bias_initializer=keras.initializers.glorot_uniform(),
+                                          name='location_output_t0',
+                                          )(hidden_state_in_0)
         #   ================
         #   Baseline Network
         #   ================
@@ -134,6 +150,9 @@ class RAM():
 
         # Create the model
         self.ram = keras.models.Model(inputs=[glimpse_model_i, location_model_i], outputs=[action_out, location_out, baseline_output])
+
+        # Create Location model at timestep 0
+        self.loc_t0 = keras.model.Model(inputs=hidden_state_in_0, output=location_out_t0)
 
         # Compile the model
         if optimizer == "rmsprop":
@@ -319,6 +338,12 @@ class RAM():
         :return:
         """
         self.ram.reset_states()
+    def start_location(self):
+        w = self.ram.get_layer("location_output").get_weights()
+        self.loc_t0.set_weights(w)
+        h0 = np.zeros((self.batch_size,256))
+        return self.loc_t0.predict_on_batch(h0)
+
 
     def choose_action(self,X,loc):
         """
